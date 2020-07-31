058c01badb066bbffd09ee8d0a9a081c66048405
Fernando Aldair <ferpool_ald@hotmail.com>
2020-07-31 16:00:33 -0500

    add horario

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './styles/Navbar.css';
 
 class Navbar extends React.Component{
     render(){
         return(
-            <div className="Navbar">
-                <div className="container-fuild">
+            <div className="Navbar row">
+                <div className="container-fuild col Navbar_logo">
                     <Link to="/" className="Navbar__brand">
-                        <span className="font-weight-light">Den</span>
-                        <span className="font-weight-bold">tal</span>
+                        <span className="font-weight-light logo">Den</span>
+                        <span className="font-weight-bold logo">tal</span>
                     </Link>
                 </div>
+            <div className="col-sm- Navbar_horario">
+                horario
+            </div>
+            <div className="col-sm- Navbar_horario">
+                horario
+            </div>
+            <div className="col-sm- Navbar_horario">
+                horario
+            </div>
             </div>
         );
     }
